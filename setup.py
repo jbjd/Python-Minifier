0bebe205427491586470d83e33573ceb331b2629
James Demetris <63110925+jbjd@users.noreply.github.com>
2024-10-18 18:27:31 -0500

    Install fix (#3)
    For some reason, moving autoflake dependency out of setup.py to pyproject fixes install via pip from git url

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ import setuptools
 NAME: str = "personal_python_minifier"
 
 setuptools.setup(
-    name=NAME,
-    packages=setuptools.find_packages(include=[NAME, f"{NAME}.*"]),
+    name=NAME, packages=setuptools.find_packages(include=[NAME, f"{NAME}.*"])
 )
