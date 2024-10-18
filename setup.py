6fe844d622d63b4c56f4394e7e6c42b66e194db8
jbjd <jimdemetris@yahoo.com>
2024-10-18 18:25:51 -0500

    Move autoflake to pyproject

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ import setuptools
 NAME: str = "personal_python_minifier"
 
 setuptools.setup(
-    name=NAME,
-    packages=setuptools.find_packages(include=[NAME, f"{NAME}.*"]),
-    install_requires=["autoflake==2.3.1"],
+    name=NAME, packages=setuptools.find_packages(include=[NAME, f"{NAME}.*"])
 )
