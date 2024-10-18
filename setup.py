da444e02628b510931d4cc764720c9eeb0008165
jbjd <jimdemetris@yahoo.com>
2024-10-18 18:21:21 -0500

    Try adding version to autoflake in setup

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ NAME: str = "personal_python_minifier"
 setuptools.setup(
     name=NAME,
     packages=setuptools.find_packages(include=[NAME, f"{NAME}.*"]),
-    install_requires=["autoflake"],
+    install_requires=["autoflake==2.3.1"],
 )
