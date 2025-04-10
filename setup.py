0629a5b029a507deaa57dee32a9e318403787fc3
jbjd <jimdemetris@yahoo.com>
2025-04-10 18:00:15 -0500

    Fix some broken references

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-NAME: str = "personal_python_ast_optimizer"
+NAME: str = "personal_python_syntax_optimizer"
 
 setuptools.setup(
     name=NAME, packages=setuptools.find_packages(include=[NAME, f"{NAME}.*"])
